d7dd5b271906cec1be4230002f4881f29919ad6a
diegopereira <diegolimapereira@gmail.com>
2017-08-21 15:20:17 -0300

    create models

--- work-at-olist/channels/models.py
+++ work-at-olist/channels/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+class Channel(models.Model):
+    name = models.CharField(max_length=256, unique=True, db_column='name', blank=True)
+
+class Categories(MPTTModel): ##########
+    name = models.CharField(max_length=256, unique=True)
+    channel = models.ForeignKey(Channel, db_column = 'channelName', blank=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
